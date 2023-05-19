adc7fcad15e5250b374e400c849f8e16e42ef1f0
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-19 12:06:19 +0530

    Update source.js

--- source.js
+++ source.js
@@ -1,36 +1,35 @@
-
-
+<script>
   const passwordForm = document.querySelector("#password-form");
   const passwordInput = document.querySelector("#password-input");
   const convertForm = document.querySelector("#convert-form");
   const urlInput = document.querySelector("#url");
   const downloadLink = document.querySelector("#download");
-  const codeBlock = document.querySelector("#code-pad");
+  const codeBlock = document.querySelector("#code");
   const copyCodeBtn = document.querySelector("#copy-code-btn");
-
+  
   passwordForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const password = passwordInput.value;
-
+  
     // Replace 'your-password' with your desired password
-    if (password === "000") {
+    if (password === "your-password") {
       passwordForm.style.display = "none";
       convertForm.style.display = "block";
     } else {
       alert("Incorrect password. Please try again.");
     }
   });
-
+  
   convertForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const url = urlInput.value;
-
+  
     fetch(url)
       .then(response => response.text())
       .then(data => {
-        // Set the textContent of the code pad to the HTML source code
+        // Set the textContent of the code block to the HTML source code
         codeBlock.textContent = data;
-
+  
         // Show the download and copy buttons
         downloadLink.innerHTML = `
           <button onclick="downloadHTML()">Download HTML</button>
@@ -38,14 +37,13 @@
           <button onclick="downloadJS()">Download JavaScript</button>
         `;
         copyCodeBtn.style.display = "inline-block";
-        codeBlock.style.display = "block";
       })
       .catch(error => {
         console.error(error);
         alert("There was an error fetching the web page. Please check the URL and try again.");
       });
   });
-
+  
   function downloadHTML() {
     const data = codeBlock.textContent;
     const blob = new Blob([data], { type: 'text/html' });
@@ -54,7 +52,7 @@
     link.download = 'converted.html';
     link.click();
   }
-
+  
   function downloadCSS() {
     const data = getCSSFromCode();
     const blob = new Blob([data], { type: 'text/css' });
@@ -63,8 +61,8 @@
     link.download = 'styles.css';
     link.click();
   }
-
-    function downloadJS() {
+  
+  function downloadJS() {
     const data = getJSFromCode();
     const blob = new Blob([data], { type: 'text/javascript' });
     const link = document.createElement('a');
@@ -72,29 +70,29 @@
     link.download = 'script.js';
     link.click();
   }
-
+  
   function getCSSFromCode() {
     const styleTags = document.getElementsByTagName('style');
     let cssCode = '';
-
+  
     for (let i = 0; i < styleTags.length; i++) {
       cssCode += styleTags[i].innerText;
     }
-
+  
     return cssCode;
   }
-
+  
   function getJSFromCode() {
     const scriptTags = document.getElementsByTagName('script');
     let jsCode = '';
-
+  
     for (let i = 0; i < scriptTags.length; i++) {
       jsCode += scriptTags[i].innerText;
     }
-
+  
     return jsCode;
   }
-
+  
   copyCodeBtn.addEventListener('click', () => {
     const range = document.createRange();
     range.selectNode(codeBlock);
@@ -104,4 +102,4 @@
     window.getSelection().removeAllRanges();
     alert('Code copied to clipboard!');
   });
-
+</script>
